5f359ed9ff26a65b0c8b6f519b9eae7d7e304b66
Husain1771-2 <202100358@stu.uob.edu.bh>
2024-11-13 13:23:19 +0300

    Added S3 Bucket

--- sst.config.ts
+++ sst.config.ts
@@ -7,6 +7,8 @@ import { OIDCForGitHubCI } from './stacks/devops/OIDCForGitHubCI';
 import { AuthStack } from './stacks/AuthStack';
 import { GrammarToolStack } from './stacks/GrammarToolStack';
 
+import { Storage } from './stacks/Storage';
+
 export default {
   config(_input) {
     return {
