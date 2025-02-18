27bbdadca076b0698220170ac81e5dcc02a9f06f
Husain Ali Yusuf <GUESTHAY@iga.gov.bh>
2025-02-18 12:34:56 +0300

    Added cdk bootstrap command

--- .codecatalyst/scripts/sst_deploy.sh
+++ .codecatalyst/scripts/sst_deploy.sh
@@ -5,7 +5,8 @@ source ~/.bashrc
 nohup dockerd &
 docker version
 npm install
-npm install react-dropzone
-npm install @heroicons/react
-npm install chart.js
+npm audit fix
+npx cdk bootstrap aws://597088060025/us-east-1 --force \
+  --cloudformation-execution-policies arn:aws:iam::597088060025:policy/Cdk_Least \
+  --no-public-access-block-configuration
 npx sst deploy --stage iga
