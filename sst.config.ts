c20a89350c4f86ceab0257b44c824313d3feeed5
Husain Ali Yusuf <GUESTHAY@iga.gov.bh>
2025-02-18 13:09:25 +0300

    Added public Access Block

--- sst.config.ts
+++ sst.config.ts
@@ -15,6 +15,7 @@ export default {
       name: 'codecatalyst-sst-app',
       region: 'us-east-1',
       cdk:{
+        publicAccessBlockConfiguration: true,
         customPermissionsBoundary: "NCSC-Boundry"
       }
     };
