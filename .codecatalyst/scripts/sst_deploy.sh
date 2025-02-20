f2ce26b620684a90f2b9a2701c40db13eb916935
Husain Ali Yusuf <GUESTHAY@iga.gov.bh>
2025-02-20 09:18:57 +0300

    Added customPermissionsBoundry to sst_deploy

--- .codecatalyst/scripts/sst_deploy.sh
+++ .codecatalyst/scripts/sst_deploy.sh
@@ -8,6 +8,6 @@ npm install
 npm audit fix
 npx cdk bootstrap aws://597088060025/us-east-1 --force \
   --cloudformation-execution-policies arn:aws:iam::597088060025:policy/Cdk_Least \
-  --custom-permissions-boundary arn:aws:iam::597088060025:policy/NCSC-Boundry \
+  --custom-permissions-boundary arn:aws:iam::597088060025:policy/NCSC_Boundry \
   --no-public-access-block-configuration
 npx sst deploy --stage iga
