0141aea6554931adf3f0c3650c425d4c344a7315
Husain Ali Yusuf <GUESTHAY@iga.gov.bh>
2025-02-25 08:19:23 +0300

    Just testing new Bucket

--- stacks/StorageStack.ts
+++ stacks/StorageStack.ts
@@ -1,18 +1,17 @@
 import { Bucket, Function, StackContext } from 'sst/constructs';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as iam from 'aws-cdk-lib/aws-iam';
-import { CfnBucket } from 'aws-cdk-lib/aws-s3'
+import { CfnBucket } from 'aws-cdk-lib/aws-s3';
+import { CfnParameter } from 'aws-cdk-lib';
 import sstConfig from '../sst.config';
+import { EncryptionConfiguration } from 'aws-cdk-lib/aws-stepfunctions';
 
 export function StorageStack({ stack }: StackContext) {
-  const boundary = new iam.ManagedPolicy(stack, "Boundary", {
-    statements: [
-      new iam.PolicyStatement({
-        effect: iam.Effect.DENY,
-        actions: ["s3:PutBucketPublicAccessBlock"],
-        resources: ["*"],
-      }),
-    ],
+  const applyPublicAccessBlock = new CfnParameter(stack, 'ApplyPublicAccessBlock', {
+    type: 'String',
+    default: 'true',  // Default to "true" (public access block applied)
+    allowedValues: ['true', 'false'],
+    description: 'Whether to apply the public access block on the S3 bucket.',
   });
 
   //Create the Lambda function
@@ -25,34 +24,46 @@ export function StorageStack({ stack }: StackContext) {
   });
 
   // Create the S3 bucket and set up notifications
-  const bucket = new Bucket(stack, 'BucketTextract', {
-    cdk:{
-      bucket:{
-        blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
-      }
-    },
-    //blockPublicACLs: true,
-    notifications: {
-      myNotification: {
-        function: notificationFunction,
-        events: ['object_created'],
-      },
-    }
-  });
-
-  // const bucket = new CfnBucket(stack, "BucketTextract", {
-  //   notificationConfiguration: {
-  //     lambdaConfigurations: [
-  //       {
-  //         event: "s3:ObjectCreated:Put",
-  //         function: notificationFunction.functionArn,
-  //       },
-  //     ]
+  // const bucket = new Bucket(stack, 'BucketTextract', {
+  //   cdk:{
+  //     bucket:{
+  //       blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+  //     }
   //   },
+  //   //blockPublicACLs: true,
+  //   notifications: {
+  //     myNotification: {
+  //       function: notificationFunction,
+  //       events: ['object_created'],
+  //     },
+  //   }
   // });
 
-  const bucket2 = new Bucket(stack, "ExtractedTXT", {});
-  iam.PermissionsBoundary.of(stack).apply(boundary);
+  const bucket = new CfnBucket(stack, "BucketTextract", {
+    notificationConfiguration: {
+      lambdaConfigurations: [
+        {
+          event: "s3:ObjectCreated:Put",
+          function: notificationFunction.functionArn,
+        },
+      ]
+    },
+  });
+
+  const bucket2 = new CfnBucket(stack, "ExtractedTXT", {
+  });
+  const testbucket = new Bucket(stack, 'MyBucket', {
+    cdk: {
+      bucket:{
+        blockPublicAccess: new s3.BlockPublicAccess({
+          blockPublicAcls: true,
+          blockPublicPolicy: true,
+          ignorePublicAcls: true,
+          restrictPublicBuckets: true,
+        }),
+      }
+    }
+  });
 
   //notificationFunction.bind([bucket2]);
   //notificationFunction.attachPermissions(["s3"]);
