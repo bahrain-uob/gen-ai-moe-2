281f837dd19bfa2abf44086b9b225b610628fef2
Husain1771-2 <202100358@stu.uob.edu.bh>
2025-03-16 12:09:06 +0300

    Update sst_deploy.sh

--- .codecatalyst/scripts/sst_deploy.sh
+++ .codecatalyst/scripts/sst_deploy.sh
@@ -2,7 +2,13 @@
 echo "Deploying project"
 
 if [ -f ../.env ]; then
+  echo ".env file found at ../.env"
   source ../.env
+elif [ -f $HOME/.env ]; then
+  echo ".env file found at $HOME/.env"
+else
+  echo ".env file not found :("
+  echo "Current working directory: $(pwd)"
 fi
 
 source ~/.bashrc
