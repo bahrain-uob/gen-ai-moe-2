4fdbaa8002dfbe1bc39cb2b1de0f199e860415da
Husain1771-2 <202100358@stu.uob.edu.bh>
2025-03-16 12:37:17 +0300

    Update sst_deploy.sh

--- .codecatalyst/scripts/sst_deploy.sh
+++ .codecatalyst/scripts/sst_deploy.sh
@@ -4,17 +4,20 @@ echo "Deploying project"
 if [ -f ./.env ]; then
   echo ".env file found in the current directory"
   source ./.env
+  #source ~/.bashrc
+  nohup dockerd &
+  docker version
+  npm install
+  npm audit fix
+  echo aws://$AWS_ACCOUNT_ID/us-east-1
+  npx cdk bootstrap aws://$AWS_ACCOUNT_ID/us-east-1 --force \
+    --cloudformation-execution-policies arn:aws:iam::$AWS_ACCOUNT_ID:policy/Cdk_Least \
+    --no-public-access-block-configuration
+  npx sst deploy --stage iga
 else
   echo ".env file not found in the current directory"
+  echo "Current directory: $(pwd)"
+  echo "Please add .env file"
+  echo "Canceling Deployment"
+  exit 1
 fi
-
-#source ~/.bashrc
-nohup dockerd &
-docker version
-npm install
-npm audit fix
-echo aws://$AWS_ACCOUNT_ID/us-east-1
-npx cdk bootstrap aws://$AWS_ACCOUNT_ID/us-east-1 --force \
-  --cloudformation-execution-policies arn:aws:iam::$AWS_ACCOUNT_ID:policy/Cdk_Least \
-  --no-public-access-block-configuration
-npx sst deploy --stage iga
