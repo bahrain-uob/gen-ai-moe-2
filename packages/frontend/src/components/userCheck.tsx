9ebaa7b01e81869e58368ef6ec8c4840c40fc223
Husain1771-2 <202100358@stu.uob.edu.bh>
2025-04-08 05:38:37 +0000

    Commented out userCheck.tsx as it keeps returning undefined in authContext

--- packages/frontend/src/components/userCheck.tsx
+++ packages/frontend/src/components/userCheck.tsx
@@ -1,22 +1,22 @@
 import { AuthContext } from '../AuthContext';
 import { useContext, useEffect } from 'react';
-import { /*Link,*/ useNavigate } from 'react-router-dom';
+// import { /*Link,*/ useNavigate } from 'react-router-dom';
 
 const AdminUserCheck = () => {
-const navigate = useNavigate();
-const authInfo = useContext(AuthContext)
+// const navigate = useNavigate();
+const authInfo = useContext(AuthContext) //This keeps returning undefined
 
 useEffect(()=>{
 const adminUserCheck = async () => {
     // Checking if user info exists in the context
-    if (authInfo) {
+    if (authInfo) { //This keeps returning undefined
       // Extract institution from the idToken payload
-      const institution = authInfo.authSession?.tokens?.idToken?.payload['custom:Institution'] as string;
-      console.log("INSTITUTION:", institution)
+      // const institution = authInfo.authSession?.tokens?.idToken?.payload['custom:Institution'] as string;
+      // console.log("INSTITUTION:", institution)
       // Redirect based on the institution
-      if (institution?.toUpperCase() != 'MOE') {
-        navigate('/');
-      }
+      // if (institution?.toUpperCase() != 'MOE') {
+      //   navigate('/');
+      // }
     };
   };
   adminUserCheck();
